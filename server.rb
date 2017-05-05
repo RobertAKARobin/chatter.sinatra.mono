b27a0fbec5dce93f75c9f6db063e17511c212703
RobertAKARobin <robertgfthomas@gmail.com>
2017-05-05 16:14:42 -0500

    Loads and displays Convos

--- server.rb
+++ server.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+require './models/convo'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
 	File.read(File.join('public', 'index.html'))
+end
+
+get '/convos' do
+	Convo.all.to_json
 end
